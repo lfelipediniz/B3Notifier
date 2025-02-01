6b7c352802d579d82b319e0f7c8c24893a2585de
Luiz Felipe <lfediniz@gmail.com>
2025-02-01 03:24:33 -0300

    feat: add currency input and time selector to EditStockModal

--- frontend/src/components/EditStockModal.jsx
+++ frontend/src/components/EditStockModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -7,11 +7,23 @@ import {
   DialogDescription,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { Plus, Settings } from "lucide-react";
+import { Plus, Clock } from "lucide-react";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import StockCard from "./StockCard";
+import CurrencyInput from "react-currency-input-field";
 
+const timeIntervals = ["5 min", "10 min", "15 min", "30 min", "1 hora"];
 
 const EditStockModal = ({ isOpen, onClose }) => {
+  const [selectedTime, setSelectedTime] = useState(timeIntervals[0]);
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="max-w-lg p-6 bg-[hsl(var(--background))] rounded-lg shadow-lg">
@@ -23,14 +35,53 @@ const EditStockModal = ({ isOpen, onClose }) => {
             </DialogTitle>
           </div>
           <DialogDescription className="text-sm text-[hsl(var(--muted-foreground))]">
-          Edite as configurações do ativo, redefina os limites e ajuste a periodicidade
+            Edite as configurações do ativo, redefina os limites e ajuste a
+            periodicidade
           </DialogDescription>
         </DialogHeader>
+
+        <CurrencyInput
+          placeholder="R$ 20,00"
+          prefix="R$ "
+          decimalSeparator=","
+          groupSeparator="."
+          decimalsLimit={2}
+          className="w-full px-4 py-2 border border-[hsl(var(--red))] rounded-md bg-[hsl(var(--background))] text-[hsl(var(--foreground))] focus:outline-none"
+        />
+
+        <CurrencyInput
+          placeholder="R$ 30,00"
+          prefix="R$ "
+          decimalSeparator=","
+          groupSeparator="."
+          decimalsLimit={2}
+          className="w-full px-4 py-2 border border-[hsl(var(--green))] rounded-md bg-[hsl(var(--background))] text-[hsl(var(--foreground))] focus:outline-none"
+        />
+
+        {/* seletor de tempo */}
+        <div className="relative mt-4">
+          <Select value={selectedTime} onValueChange={setSelectedTime}>
+            <SelectTrigger className="flex items-center w-full gap-2 px-4 py-2">
+              <Clock className="w-4 h-4 text-gray-500" />{" "}
+              {/* Ícone à esquerda */}
+              <SelectValue placeholder="Selecione o tempo..." />
+            </SelectTrigger>
+            <SelectContent className="w-48">
+              {timeIntervals.map((time) => (
+                <SelectItem key={time} value={time}>
+                  {time}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+
+        <StockCard ticker="ITUB4" price={29.0} buy={25.0} sell={30.0} />
 
         <div className="mt-4 border-t pt-4 border-[hsl(var(--border))]">
           <div className="flex items-center justify-between">
             <Button className="bg-[hsl(var(--primary))] text-[hsl(var(--primary-foreground))]">
-              Adicionar
+              Alterar
             </Button>
           </div>
         </div>
